beadbc65789a4fa2fae99e4fc8a4581616b1371d
Andrey <Andreikoman@gmail.com>
2020-12-02 17:09:23 +0300

    File_Manager

---
+++ admin/uploader.php
@@ -0,0 +1,22 @@
+<?php 
+
+if (empty($_FILES['files']['name'][0])) exit;
+
+$destPath = '../uploads';
+if (!file_exists($destPath)) {
+    mkdir('../uploads');
+}
+
+$allFiles = scandir($destPath);
+
+foreach($_FILES['files']['tmp_name'] as $index => $path) {
+    if (file_exists($path)) {
+        $fileInfo = pathinfo($_FILES['files']['name'][$index]);
+
+        $findFiles = preg_grep("/^" . $fileInfo['filename'] . "(.+)?\." . $fileInfo['extension'] . "$/", $allFiles);
+        $fileName = $fileInfo['filename'] . (count($findFiles) > 0 ? '_' . (count($findFiles)+1) : '') . '.' . $fileInfo['extension'];
+
+        move_uploaded_file($path, $destPath . '/' . $fileName);
+    }
+}
+header("Location: ./explorer.php?path=./../uploads/");
